a77e1dbf992c5e4652b416b2f2571544ba26151b
tranhuyhoangbka <hoangth92.nd@gmail.com>
2022-01-27 07:22:03 +0700

    App 2:  html construct

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -31,6 +31,36 @@ function App2() {
           </div>
         </div>
       </header>
+
+      <main>
+        <section className='hero'>
+          <div className='container'>
+            <div className='hero-main'>
+              <div className='hero-content'>
+                <h1 class='heading hero-heading'>Explore and Travel</h1>
+                <div className='hero-caption'>Holiday finder</div>
+                <div className='hero-form'>
+                <div class="dropdown">
+                  <div class="dropdown-select">
+                    <span class="dropdown-value">Location</span>
+                    <i class='fal fa-angle-down'></i>
+                  </div>
+                  <div class="dropdown-list">
+                    <div class="dropdown-item">title</div>
+                    <div class="dropdown-item">title</div>
+                    <div class="dropdown-item">title</div>
+                  </div>
+                </div>
+                </div>
+                <a className='btn btn--primary' href=''>Explore</a>
+              </div>
+              <div className='hero-image'>
+                <img srcSet='/images/app2/img1.png 2x' alt='' />
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
