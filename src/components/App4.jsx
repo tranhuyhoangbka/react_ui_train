0ec66015eb7c49bde7f36f6268d49da0fba497e4
tranhuyhoangbka <hoangth92.nd@gmail.com>
2022-03-27 18:04:12 +0700

    App4: Drag drop element

--- src/components/App4.jsx
+++ src/components/App4.jsx
@@ -20,6 +20,8 @@ export const App4 = () => {
     // });
     // // }, 0);
 
+    // start hide show password
+
     const toggle = document.querySelectorAll('.eye');
     toggle.forEach(element => {
       element.addEventListener('click', function() {
@@ -34,6 +36,48 @@ export const App4 = () => {
         }
       });
     }); 
+    // end hide show password
+
+    // start drag drop
+    const targets = document.querySelectorAll('.target');
+    const boxes = document.querySelectorAll('.box');
+
+    // target.addEventListener('dragstart', function(e) {
+    //   this.classList.add('dragging');
+    // });
+
+    // target.addEventListener('dragend', function(e) {
+    //   this.classList.remove('dragging');
+    // });
+    let currentTarget = null;
+
+    targets.forEach(element => {
+      element.addEventListener('dragstart', function(e) {
+        currentTarget = this;
+        this.classList.add('dragging');
+      });
+
+      element.addEventListener('dragend', function(e) {
+        this.classList.remove('dragging');
+      });
+    });
+
+    boxes.forEach(box => {
+      box.addEventListener('dragover', function(e) {
+        e.preventDefault();
+        if(!this.querySelector('.target')) {
+          this.appendChild(currentTarget);
+        }
+      });
+
+      box.addEventListener('drop', function(e) {
+        if(!this.querySelector('.target')) {
+          this.appendChild(currentTarget);
+        }
+      });
+    });
+    
+    // end drag drop
     
   }, []);
 
@@ -77,6 +121,18 @@ export const App4 = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
           </svg>
         </div>
+      </div>
+
+      <div className='dragdropWrapper'>
+        <div className="box">
+          <div className="target" draggable>abc1</div>
+        </div>
+        <div className="box">
+          <div className="target" draggable>xyz1</div>
+        </div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
       </div>
     </div>
   )
